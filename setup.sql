776301dd56065674ae3f05603cba239bb2e0ed14
tylermarshall7 <tyler.marshall7@gmail.com>
2020-09-02 14:20:08 -0600

    init

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
-/* CREATE TABLE cars (
+CREATE TABLE cars (
     id INT NOT NULL AUTO_INCREMENT,
     make VARCHAR(255) NOT NULL,
     model VARCHAR(255) NOT NULL,
@@ -8,11 +8,9 @@
     description VARCHAR(255),
     userId VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
-) */
+)
 
-
-/* 
 INSERT INTO cars
 (make, model, year, price, imgUrl, description, userId)
 VALUES
-("Chevy", "Tracker", 1989, 2000, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiEdhq7a4aHGvv8FxjKUpAg_godVfcyLzHSg&usqp=CAU", "Itsa trackin", "D$SUPERSECRETUSERID") */
+("Chevy", "Tracker", 1989, 2000, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiEdhq7a4aHGvv8FxjKUpAg_godVfcyLzHSg&usqp=CAU", "Itsa trackin", "D$SUPERSECRETUSERID")
